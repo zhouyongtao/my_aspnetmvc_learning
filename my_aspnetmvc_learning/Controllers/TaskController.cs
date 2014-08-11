333c8857a0502600f28abc8aba2b4724449325a3
irving <zhoyongtao@outlook.com>
2014-08-11 19:20:39 +0800

    update task

--- my_aspnetmvc_learning/Controllers/TaskController.cs
+++ my_aspnetmvc_learning/Controllers/TaskController.cs
@@ -1,8 +1,10 @@
 ﻿using NLog;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -50,11 +52,22 @@ namespace my_aspnetmvc_learning.Controllers
             return Directory.GetFiles(str, "*.txt", SearchOption.AllDirectories);
         }
 
-
-        public static string Test()
+        public ActionResult Test()
         {
-            //System.Threading.Tasks.Parallel
-            return string.Empty;
+            var watch = Stopwatch.StartNew();
+            watch.Start();
+            Parallel.Invoke(
+               () =>
+               {
+                   Thread.Sleep(5000);
+               },
+               () =>
+               {
+                   Thread.Sleep(4000);
+               }
+               );
+            return Content(watch.ElapsedMilliseconds.ToString());
         }
+
     }
 }
