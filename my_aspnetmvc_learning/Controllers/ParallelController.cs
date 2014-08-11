7429e2fce72e656f4295b8f1703a4acc427684bd
Irving <zhouyongtao@outlook.com>
2014-08-11 23:10:43 +0800

    test  ParallelOptions

--- my_aspnetmvc_learning/Controllers/ParallelController.cs
+++ my_aspnetmvc_learning/Controllers/ParallelController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +13,17 @@ namespace my_aspnetmvc_learning.Controllers
         // GET: Parallel
         public ActionResult Index()
         {
+            var options = new ParallelOptions();
+            //指定使用的硬件线程数为1
+            options.MaxDegreeOfParallelism = 1;
+            Parallel.For(0, 20000000, options,(i,state) =>
+                    {
+                        if (i == 1000)
+                        {
+                            state.Break();
+                            return;
+                        }
+                    });
             return View();
         }
     }
