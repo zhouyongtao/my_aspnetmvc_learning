51137e75084de1a81bf640a217269231c95a49f8
Irving <zhouyongtao@outlook.com>
2014-08-11 21:55:50 +0800

    add Parallel controller

---
+++ my_aspnetmvc_learning/Controllers/ParallelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_aspnetmvc_learning.Controllers
+{
+    public class ParallelController : Controller
+    {
+        // GET: Parallel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
