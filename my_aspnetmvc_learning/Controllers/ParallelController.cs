2b55773c62dc451f4991d7c3d356b7402d20316e
Irving <zhouyongtao@outlook.com>
2014-08-11 23:43:53 +0800

    test  ParallelOptions

--- my_aspnetmvc_learning/Controllers/ParallelController.cs
+++ my_aspnetmvc_learning/Controllers/ParallelController.cs
@@ -15,9 +15,9 @@ namespace my_aspnetmvc_learning.Controllers
         {
             var options = new ParallelOptions {MaxDegreeOfParallelism = 1};
             //指定使用的硬件线程数为1
-            Parallel.For(0, 20000000, options,(i,state) =>
+            Parallel.For(0, 10000, options,(i,state) =>
                     {
-                        if (i == 1000)
+                        if (i == 100)
                         {
                             state.Break();
                             return;
