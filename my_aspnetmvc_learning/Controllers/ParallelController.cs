2098c36163e29e317b9c4d5a8ac6963a84f3e507
irving <zhoyongtao@outlook.com>
2014-08-15 14:39:39 +0800

    update task

--- my_aspnetmvc_learning/Controllers/ParallelController.cs
+++ my_aspnetmvc_learning/Controllers/ParallelController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,13 @@ namespace my_aspnetmvc_learning.Controllers
 {
     public class ParallelController : Controller
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         // GET: Parallel
         public ActionResult Index()
         {
-            var options = new ParallelOptions {MaxDegreeOfParallelism = 1};
+            var options = new ParallelOptions { MaxDegreeOfParallelism = 1 };
             //指定使用的硬件线程数为1
-            Parallel.For(0, 10000, options,(i,state) =>
+            Parallel.For(0, 10000, options, (i, state) =>
                     {
                         if (i == 100)
                         {
@@ -24,6 +26,33 @@ namespace my_aspnetmvc_learning.Controllers
                         }
                     });
             return View();
+        }
+        public ActionResult dict()
+        {
+            var dictParallelDays = new ConcurrentDictionary<string, string>();
+            try
+            {
+                System.Threading.Tasks.Parallel.For(0, 35, i => dictParallelDays.TryAdd(DateTime.Now.AddDays(i).ToString("yyyy-MM-dd"), DateTime.Now.AddDays(i + 1).ToString("yyyy-MM-dd")));
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var single in ex.InnerExceptions)
+                {
+                    //logger.Error("HanTingServiceJob AggregateException: " + ex.Message);
+                }
+            }
+            var dictDays = dictParallelDays.AsParallel().OrderBy((data => data.Key));
+            dictDays.ForEach(data =>
+            {
+                string key = data.Key;
+                string value = data.Value;
+            });
+            //Parallel
+            Parallel.ForEach(dictParallelDays, item =>
+            {
+                Logger.Info("key: " + item.Key + "  value : " + item.Value);
+            });
+            return Content(dictDays.Count().ToString());
         }
     }
 }
