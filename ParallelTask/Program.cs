22bc9fa46ae5ab74072eea36732a982e7dbc755d
Irving <zhouyongtao@outlook.com>
2014-08-16 23:42:09 +0800

    update GetDayOfWeek

--- ParallelTask/Program.cs
+++ ParallelTask/Program.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ParallelTask
 {
-    class Program
+    public class Program
     {
+        private static readonly Action<string> logger = Console.WriteLine;
         static void Main(string[] args)
         {
+            GetDayOfWeek();
+            Console.ReadKey();
+        }
+        public static void GetDayOfWeek()
+        {
+            //运行在另外一个线程中
+            var dayName = Task.Run<string>(() =>
+            {
+                logger("wating");
+                Thread.Sleep(2000);
+                return new DateTime().DayOfWeek.ToString();
+            });
+            logger("今天是: " + dayName.Result);
         }
     }
 }
