5ac8ba8f71f5d74eaa21bd3a9e7d7ec73e0e8b67
Irving <zhouyongtao@outlook.com>
2014-08-17 18:10:03 +0800

    add AsyncController

--- my_aspnetmvc_learning/Controllers/AsyncController.cs
+++ my_aspnetmvc_learning/Controllers/AsyncController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +12,39 @@ namespace my_aspnetmvc_learning.Controllers
     public class AsyncController : Controller
     {
         // GET: Async
-        public ActionResult Index()
+        [AsyncTimeout(1000)]
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var data = await GetPageTaskAsync("http://163.com");
+            string otherData = "Irving";
+            return data;
+        }
+
+        /// <summary>
+        /// 处理异步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<ActionResult> GetPageTaskAsync(string url)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    await Task.Delay(3000);
+                    var fetchTextTask = client.GetStringAsync(url);
+                    return Json(new
+                    {
+                        fetchText = await fetchTextTask,
+                        error = "NO"
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (WebException exception)
+            {
+                throw exception;
+                // TODO: Logging, update statistics etc.
+            }
         }
     }
 }
