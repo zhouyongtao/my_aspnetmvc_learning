f22fe021411146662bbd74440f0b86464a8e9cf3
Irving <zhouyongtao@outlook.com>
2014-08-17 15:52:04 +0800

    add AsyncController

---
+++ my_aspnetmvc_learning/Controllers/AsyncController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_aspnetmvc_learning.Controllers
+{
+    public class AsyncController : Controller
+    {
+        // GET: Async
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
