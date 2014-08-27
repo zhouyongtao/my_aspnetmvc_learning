8e1eb6b2becee80cdec579ad456398e98275dec8
irving <zhouyongtao@outlook.com>
2014-08-27 18:02:03 +0800

    add RedirectController

---
+++ my_aspnetmvc_learning/Controllers/RedirectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_aspnetmvc_learning.Controllers
+{
+    public class RedirectController : Controller
+    {
+        // GET: /Redirect/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
