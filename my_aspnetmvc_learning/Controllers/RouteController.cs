07d5115d07d99dce2ee234060dcc864bbec526ee
irving <zhouyongtao@outlook.com>
2014-09-25 11:04:36 +0800

    add route

--- my_aspnetmvc_learning/Controllers/RouteController.cs
+++ my_aspnetmvc_learning/Controllers/RouteController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 
 namespace my_aspnetmvc_learning.Controllers
 {
+    /// <summary>
+    /// http://diaosbook.com/Post/2013/10/22/attribute-routing-in-asp-net-mvc-5
+    /// </summary>
     public class RouteController : Controller
     {
         //
@@ -14,5 +17,27 @@ namespace my_aspnetmvc_learning.Controllers
         {
             return View();
         }
-	}
+        // eg: /books
+        // eg: /books/1430210079
+        [Route("books/{isbn?}")]
+        public ActionResult View(string isbn)
+        {
+            return View();
+        }
+
+        // eg: /books/lang
+        // eg: /books/lang/en
+        // eg: /books/lang/he
+        [Route("books/lang/{lang=en}")]
+        public ActionResult ViewByLanguage(string lang)
+        {
+      
+            return View();
+        }
+        [Route("attribute-routing-in-asp-net-mvc-5")]
+        public ActionResult Hotel()
+        {
+            return Content("http://diaosbook.com/Post/2013/10/22/attribute-routing-in-asp-net-mvc-5");
+        }
+    }
 }
