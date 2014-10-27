85c0d2db001e799bd5f6b9c6ba4f2c1dd6b99a78
irving <zhouyongtao@outlook.com>
2014-10-27 11:04:16 +0800

    add  HangFireDB

---
+++ my_aspnetmvc_learning/Filters/AsyncLoggingFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_aspnetmvc_learning.Filters
+{
+    public class AsyncLoggingFilter : ActionFilterAttribute
+    {
+      
+
+    }
+}
--- my_aspnetmvc_learning/Startup.cs
+++ my_aspnetmvc_learning/Startup.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.Owin;
 using Owin;
-
+using Hangfire;
+using Hangfire.SqlServer;
 [assembly: OwinStartupAttribute(typeof(my_aspnetmvc_learning.Startup))]
 namespace my_aspnetmvc_learning
 {
     public partial class Startup
     {
+        private readonly string HangFireDB = @"Data Source=.;Initial Catalog=HangFireDB;User ID=sa;Password=123";
         public void Configuration(IAppBuilder app)
         {
+            app.UseHangfire(config =>
+            {
+                config.UseSqlServerStorage(HangFireDB);
+                config.UseServer();
+
+            });
+            RecurringJob.AddOrUpdate(() => TestJob(), Cron.Daily);
             ConfigureAuth(app);
+        }
+
+        public void TestJob()
+        {
+
         }
     }
 }
